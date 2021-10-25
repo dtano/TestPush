1866feb49bdfe17903a6d3f483f105e15f90d985
Denzel Tano <denzel.tano@unit4.com>
2021-10-25 11:25:03 +0700

    Test commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot sampleRobot = new Robot("Marianne");
+            sampleRobot.Greet();
+        }
+    }
+
+    public class Robot
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+
+        public Robot(string name)
+        {
+            Name = name;
+            Hp = 30;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Hello my name is {Name}");
+        }
+
+        public void Damage(int dmgAmount)
+        {
+            Console.WriteLine("Robot is dead");
+            Hp -= dmgAmount;
+        }
+    }
+}
